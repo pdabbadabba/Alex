d8926e97554f79fe6fd3a1bc5d394117a950c78d
Paul Caritj <caritj@gmail.com>
2014-02-16 13:55:06 -0500

    Implemented rolling checksum, more efficient signature data structure.

--- src/CoarsePatch/__init__.py
+++ src/CoarsePatch/__init__.py
@@ -4,28 +4,37 @@ import struct
 from base64 import b64encode
 import itertools
 import zlib
-from collections import deque
+from collections import deque, defaultdict
 from os import path
 import gzip
+from hashlib import md5 as HASH
+import pprint
 
 KB = 1024
 MB = 1000*KB
 
+if not(hasattr(__builtins__, "bytes")) or str is bytes:
+    # Python 2.x compatibility
+    def bytes(var, *args):
+        try:
+            return ''.join(map(chr, var))
+        except TypeError:
+            return map(ord, var)
+
 def rollingchecksum(removed, new, a, b, blocksize=4096):
-    """
-    Generates a new weak checksum when supplied with the internal state
-    of the checksum calculation for the previous window, the removed
-    byte, and the added byte.
-    """
+
+    removed = ord(removed)
+    new = ord(new)
+
     a -= removed - new
     b -= removed * blocksize - a
     return (b << 16) | a, a, b
 
 
 def weakchecksum(data):
-    """
-    Generates a weak checksum from an iterable set of bytes.
-    """
+
+    data = bytes(data)
+
     a = b = 0
     l = len(data)
     for i in range(l):
@@ -35,52 +44,79 @@ def weakchecksum(data):
     return (b << 16) | a, a, b
 
 def generate_sigs(file_name, block_size):
-    sig_list = []
-    sig = ''
+    sig_dict = defaultdict(dict)
 
     with open(file_name, 'rb') as file_bytes:
+
         position = 0
+
         for block in iter(lambda: file_bytes.read(block_size), ''):
-            block_sig = zlib.adler32(block)
-            sig_list.append([block_sig, len(block), block[:5], block[-5:], position])
+
+            c, a,b = weakchecksum(block)
+
+            sig_dict[c][HASH(block).hexdigest()] = [position, len(block)]
             position += block_size
 
-    data = {'blocks': sig_list, 'signature': zlib.adler32(str(sig_list))}
-    return data
+
+    return sig_dict
 
 def find_matches(new_file_name, old_sigs, block_size):
 
     patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
 
-    new_bytes = ""
+    new_bytes = []
 
     with open(new_file_name, 'rb') as file_bytes, gzip.open(patch_file_name, 'wb') as patch_bytes:
 
-        q  = file_bytes.read(block_size)
+        q  = deque(file_bytes.read(block_size))
+        c,a,b = weakchecksum(q)
 
         for byte in iter(lambda: file_bytes.read(1), ''):
 
-            match = False
 
-            for sig, size, start, end, position in old_sigs:
+            # Check for matching block
+            sig_hash = None
+            match = None
+            weak_match = None
+            weak_match = old_sigs.get(c)
+            if weak_match:
 
-                if ((start == q[:5]) and (end == q[-5:])) and zlib.adler32(q) == sig:
+                match = weak_match.get(HASH(''.join(q)).hexdigest())
+                #print weak_match[HASH(''.join(q)).hexdigest()]
+                #print weak_match[HASH(''.join(q)).hexdigest()]
 
-                    if len(new_bytes) > 0:
-                        print >> patch_bytes, struct.pack('<ci%ds' % (len(new_bytes)), 'n', (len(new_bytes)), new_bytes)
-                        new_bytes = ""
+            # If there is a match...
+            if match:
 
-                    print >> patch_bytes, struct.pack('<cii', 's', position, block_size)
-                    q = byte + file_bytes.read(block_size-1)
-                    match = True
+                # If we had accumulated new data, write it out
+                if len(new_bytes) > 0:
+                    new_bytes_str = "".join(new_bytes)
+                    print >> patch_bytes, struct.pack('<ci%ds' % (len(new_bytes_str)), 'n', (len(new_bytes_str)), new_bytes_str)
+                    new_bytes = []
 
-            if not match:
-                new_bytes += q[:1]
-                q = q[1:]
-                q += byte
+                print >> patch_bytes, struct.pack('<cii', 's', match[0], match[1])
 
-        new_bytes += q
-        print >> patch_bytes, struct.pack('<ci%ds' % (len(new_bytes)), 'n', len(new_bytes), new_bytes)
+                # refill the queue
+                q=deque(byte + file_bytes.read(block_size-1))
+                c,a,b = weakchecksum(q)
+
+
+            else:
+
+                # roll the window
+                old_byte = q.popleft()
+                q.append(byte)
+
+                # add old byte to bytes to be written
+                new_bytes.append(old_byte)
+
+                # roll the checksum
+                c,a,b = rollingchecksum(old_byte, byte, a, b, block_size)
+
+        # write trailing, unmatched data
+        if len(new_bytes) > 0 or len(q) > 0:
+            new_bytes_str = "".join([''.join(new_bytes), ''.join(q)])
+            print >> patch_bytes, struct.pack('<ci%ds' % (len(new_bytes_str)), 'n', len(new_bytes_str), new_bytes_str)
 
 
 def patch(old_file_name, sigs, patch_file_name):
@@ -124,22 +160,38 @@ def patch(old_file_name, sigs, patch_file_name):
 
 if __name__=='__main__':
 
-    block_size = 1*KB
+    block_size = 1 * KB
+    pp = pprint.PrettyPrinter(depth=6)
 
 #    old_file_name = '../test/doc_v1.doc'
 #    new_file_name = '../test/doc_v2.doc'
 
-    old_file_name = '../test/txt_v1.txt'
-    new_file_name = '../test/txt_v2.txt'
+    old_file_name = '../test/doc_v1.doc'
+    new_file_name = '../test/doc_v2.doc'
     patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
-
-
-
     sigs = generate_sigs(old_file_name, block_size)
-    find_matches(new_file_name, sigs['blocks'], block_size)
+    find_matches(new_file_name, sigs, block_size)
 
-    patch(old_file_name, sigs, patch_file_name)
-    print len(sigs['blocks'])
+    old_file_name = '../test/doc_v1.doc'
+    new_file_name = '../test/doc_v2.doc'
+    patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
+    sigs = generate_sigs(old_file_name, block_size)
+    find_matches(new_file_name, sigs, block_size)
+
+    old_file_name = '../test/v1.html'
+    new_file_name = '../test/v2.html'
+    patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
+    sigs = generate_sigs(old_file_name, block_size)
+    find_matches(new_file_name, sigs, block_size)
+
+    old_file_name = '../test/v1.crypt'
+    new_file_name = '../test/v2.crypt'
+    patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
+    sigs = generate_sigs(old_file_name, block_size)
+    find_matches(new_file_name, sigs, block_size)
+
+    #patch(old_file_name, sigs, patch_file_name)
+    #print len(sigs['blocks'])
 
 
 
