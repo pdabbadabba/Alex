a6e3e0d40bdafbc3e93d079438266b11b3f46b98
Paul Caritj <caritj@gmail.com>
2014-02-15 20:11:05 -0500

    About to implement proper rolling checksum

--- src/CoarsePatch/__init__.py
+++ src/CoarsePatch/__init__.py
@@ -11,6 +11,28 @@ import gzip
 KB = 1024
 MB = 1000*KB
 
+def rollingchecksum(removed, new, a, b, blocksize=4096):
+    """
+    Generates a new weak checksum when supplied with the internal state
+    of the checksum calculation for the previous window, the removed
+    byte, and the added byte.
+    """
+    a -= removed - new
+    b -= removed * blocksize - a
+    return (b << 16) | a, a, b
+
+
+def weakchecksum(data):
+    """
+    Generates a weak checksum from an iterable set of bytes.
+    """
+    a = b = 0
+    l = len(data)
+    for i in range(l):
+        a += data[i]
+        b += (l - i)*data[i]
+
+    return (b << 16) | a, a, b
 
 def generate_sigs(file_name, block_size):
     sig_list = []
@@ -43,11 +65,13 @@ def find_matches(new_file_name, old_sigs, block_size):
             for sig, size, start, end, position in old_sigs:
 
                 if ((start == q[:5]) and (end == q[-5:])) and zlib.adler32(q) == sig:
+
                     if len(new_bytes) > 0:
                         print >> patch_bytes, struct.pack('<ci%ds' % (len(new_bytes)), 'n', (len(new_bytes)), new_bytes)
                         new_bytes = ""
-                    print >> patch_bytes, struct.pack('<ci', 's', sig)
-                    q = file_bytes.read(block_size)
+
+                    print >> patch_bytes, struct.pack('<cii', 's', position, block_size)
+                    q = byte + file_bytes.read(block_size-1)
                     match = True
 
             if not match:
@@ -64,7 +88,27 @@ def patch(old_file_name, sigs, patch_file_name):
     patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
     out_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.out')
 
-    #with open(old_file_name, 'rb') as old_bytes, open(patch_file_name, 'rb') as patch_bytes, open(out_file_name, 'wb') as out_bytes:
+    #with open(old_file_name, 'rb') as old_bytes, gzip.open(patch_file_name, 'rb') as patch_bytes, open(out_file_name, 'wb') as out_bytes:
+    with gzip.open(patch_file_name, 'rb') as patch_bytes:
+
+        for data_type_bytes in iter(lambda: patch_bytes.read(struct.calcsize('<c')), ""):
+
+            print '---'
+
+            data_type = struct.unpack('<c', data_type_bytes)[0]
+            print data_type
+
+            if data_type == 's':
+                position, size = struct.unpack('<ii', patch_bytes.read(struct.calcsize('<ii')))
+                print position
+                print size
+            elif data_type == 'n':
+                length = struct.unpack('<i', patch_bytes.read(struct.calcsize('<i')))[0]
+                data = struct.unpack('<%ds' % length, patch_bytes.read(struct.calcsize('<%ds' % length)))[0]
+                print data
+
+
+            patch_bytes.read(struct.calcsize('<c'))
 
         # If signature
         # ... lookup signature's position in old
@@ -80,16 +124,22 @@ def patch(old_file_name, sigs, patch_file_name):
 
 if __name__=='__main__':
 
-    block_size = 50*KB
+    block_size = 1*KB
+
+#    old_file_name = '../test/doc_v1.doc'
+#    new_file_name = '../test/doc_v2.doc'
+
+    old_file_name = '../test/txt_v1.txt'
+    new_file_name = '../test/txt_v2.txt'
+    patch_file_name = path.join(path.split(new_file_name)[0],path.split(new_file_name)[1] + '.patch')
 
 
-
-    old_file_name = '../test/doc_v1.doc'
-    new_file_name = '../test/doc_v1.doc'
 
     sigs = generate_sigs(old_file_name, block_size)
     find_matches(new_file_name, sigs['blocks'], block_size)
 
+    patch(old_file_name, sigs, patch_file_name)
+    print len(sigs['blocks'])
 
 
 
